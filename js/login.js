61525748694290b7d5799eb74f842ca39676590d
EricERodriguez <eric92rodriguez@gmail.com>
2021-08-02 20:45:08 -0300

    footer and navbar

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-function store(){
+function store() {
 
     var name = document.getElementById('name');
     var pw = document.getElementById('pw');
@@ -6,36 +6,37 @@ function store(){
     var upperCaseLetters = /[A-Z]/g;
     var numbers = /[0-9]/g;
 
-    if(name.value.length == 0){
+    if (name.value.length == 0) {
         alert('Por favor rellenar Email');
 
-    }else if(pw.value.length == 0){
+    } else if (pw.value.length == 0) {
         alert('Por favor rellenar password');
 
-    }else if(name.value.length == 0 && pw.value.length == 0){
+    } else if (name.value.length == 0 && pw.value.length == 0) {
         alert('Por favor rellenar email y password');
 
-    }else if(pw.value.length > 8){
+    } else if (pw.value.length > 8) {
         alert('Maximo 8 caracteres');
 
-    }else if(!pw.value.match(numbers)){
+    } else if (!pw.value.match(numbers)) {
         alert('Debe contener 1 numero');
 
-    }else if(!pw.value.match(upperCaseLetters)){
+    } else if (!pw.value.match(upperCaseLetters)) {
         alert('Debe contener 1 mayuscula');
 
-    }else if(!pw.value.match(lowerCaseLetters)){
+    } else if (!pw.value.match(lowerCaseLetters)) {
         alert('Debe contener 1 minuscula');
 
-    }else{
+    } else {
         localStorage.setItem('name', name.value);
         localStorage.setItem('pw', pw.value);
         alert('Su cuenta ha sido creada con exito!');
     }
 }
 
+
 //checking
-function check(){
+function check() {
     var storedName = localStorage.getItem('name');
     var storedPw = localStorage.getItem('pw');
 
@@ -43,9 +44,9 @@ function check(){
     var userPw = document.getElementById('userPw');
     var userRemember = document.getElementById("rememberMe");
 
-    if(userName.value == storedName && userPw.value == storedPw){
+    if (userName.value == storedName && userPw.value == storedPw) {
         alert('Se ha logueado');
-    }else{
+    } else {
         alert('Error al loguear');
     }
 }
--- js/sistemaUsuarios/registro.js
+++ js/sistemaUsuarios/registro.js
@@ -1,21 +1,21 @@
 // Se crea un objeto con el usuario administrador
 const admin = {
-    nombre: 'Admin',
-    contraseña :'admin',
-    email:'none'
-}
-// Se piden los datos de los usuarios del local storage, si estos no existen, asignamos el valor de un array con el usuario administrador
+        nombre: 'Admin',
+        contraseña: 'admin',
+        email: 'none'
+    }
+    // Se piden los datos de los usuarios del local storage, si estos no existen, asignamos el valor de un array con el usuario administrador
 const usuarios = JSON.parse(localStorage.getItem('usuariosKey')) || [admin]
 
 // Seteamos el localstorage
 localStorage.setItem('usuariosKey', JSON.stringify(usuarios))
 
 // Creamos una clase para nuestros usuarios
-class Usuario{
-    constructor(nombre, email, contraseña){
-        this.nombre=nombre,
-        this.email=email,
-        this.contraseña=contraseña
+class Usuario {
+    constructor(nombre, email, contraseña) {
+        this.nombre = nombre,
+            this.email = email,
+            this.contraseña = contraseña
     }
 }
 
@@ -28,22 +28,22 @@ const crearUsuario = (e) => {
     let nombre = document.getElementById('nombre').value
     let email = document.getElementById('email').value
     let contraseña = document.getElementById('contraseña').value
-    
+
     // Consultamos si los datos no estan vacios y creamos el usuario
-    if(
-        nombre!== '' &&
-        email!== '' &&
-        email!== '' 
-    ){
+    if (
+        nombre !== '' &&
+        email !== '' &&
+        email !== ''
+    ) {
         const usuario = new Usuario(nombre, email, contraseña)
-    
+
         // Pusheamos el usuario al array de usuarios y lo guardamos en el localStorage
         usuarios.push(usuario)
         localStorage.setItem('usuariosKey', JSON.stringify(usuarios))
 
         // Redirigimos al login
         window.location.replace('/pages/login.html')
-    }else{
+    } else {
         console.error('Faltan datos')
     }
 }
